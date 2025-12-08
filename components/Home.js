b1cb65729e6ee5877fb70d1ab9e531b7bcd3e227
Thomas Morini <thomas.morini@icloud.com>
2025-12-08 15:35:15 +0100

    first draft Homepage

--- components/Home.js
+++ components/Home.js
@@ -2,12 +2,24 @@ import styles from '../styles/Home.module.css';
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <span className={styles.elCalculador}>El Calculador</span>
+      </div>
+        <div className={styles.container}>
+          <img className={styles.cover} src='cook.jpg'/>
+          <div className={styles.sign}>
+            <div className={styles.slogan}>
+              <h1>Calculez. Cuisinez. Vendez.</h1>
+            </div>
+            <div className={styles.actions}>
+              <h2>Déjà membre?</h2>
+              <button className={styles.signButtons}>Connectez vous</button>
+              <h2>Première visite?</h2>
+              <button className={styles.signButtons}>S'inscrire</button>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
